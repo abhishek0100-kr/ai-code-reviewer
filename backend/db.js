6e48106c731f86a00a96c1a14a7cf49a0ec977c3
Abhishek kumar <abhishek.kumar2024c@vitstudent.ac.in>
2026-06-21 01:25:50 +0530

    Refactor Prisma client initialization

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,16 @@ const { Pool } = require('pg');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
 
-const pool = new Pool({ connectionString: process.env.DIRECT_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = global;
+
+let prisma;
+
+if (!globalForPrisma.prisma) {
+  const pool = new Pool({ connectionString: process.env.DIRECT_URL });
+  const adapter = new PrismaPg(pool);
+  globalForPrisma.prisma = new PrismaClient({ adapter });
+}
+
+prisma = globalForPrisma.prisma;
 
 module.exports = prisma;
